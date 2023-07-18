faba76ede4201d20b475c3314b9232e3883b55dc
Shaikh juned <sheikhjuned770@gmail.com>
2023-07-18 14:23:21 +0530

    fix

--- src/features/auth/authApi.jsx
+++ src/features/auth/authApi.jsx
@@ -4,9 +4,10 @@ export function createUser(userData) {
   const response = await  fetch ("http://localhost:8080/users" , {
     method:"POST",
     body:JSON.stringify(userData),
-    headers:{ 'content-type':'aplication/json'}
+    headers:{ 'content-type':'application/json'}
   })
   const data = await response.json()
+  //TODO: on server it will only return sone info of user (not password)
   resolve({data})
   }
   );
